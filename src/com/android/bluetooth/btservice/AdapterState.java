729a5e60dad94954c7bfb204f3102a9afee41545
Alexandre <alexandre.bruxelle@zenika.com>
2016-10-27 16:22:04 +0200

    reapply right TIMEOUT
    Change-Id: I9ff313bc85272320a9857e9dedaeb01611ebe59b

--- src/com/android/bluetooth/btservice/AdapterState.java
+++ src/com/android/bluetooth/btservice/AdapterState.java
@@ -60,11 +60,11 @@ final class AdapterState extends StateMachine {
     static final int USER_TURN_OFF_DELAY_MS=500;
 
     //TODO: tune me
-    private static final int ENABLE_TIMEOUT_DELAY = 80000;
-    private static final int DISABLE_TIMEOUT_DELAY = 80000;
-    private static final int START_TIMEOUT_DELAY = 50000;
-    private static final int STOP_TIMEOUT_DELAY = 50000;
-    private static final int PROPERTY_OP_DELAY =20000;
+    private static final int ENABLE_TIMEOUT_DELAY = 8000;
+    private static final int DISABLE_TIMEOUT_DELAY = 8000;
+    private static final int START_TIMEOUT_DELAY = 5000;
+    private static final int STOP_TIMEOUT_DELAY = 5000;
+    private static final int PROPERTY_OP_DELAY =2000;
     private AdapterService mAdapterService;
     private AdapterProperties mAdapterProperties;
     private PendingCommandState mPendingCommandState = new PendingCommandState();
